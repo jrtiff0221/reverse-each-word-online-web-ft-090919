1ac9ae85bfdbcfff15f92c3d925f9c33bc91baa4
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-14 18:44:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each(sentence)
-  sentence.split.each
+  sentence.split.each { |word| word.reverse}
   
 end
 
@@ -14,16 +14,8 @@ puts "test_sentence ="
 puts test_sentance
 
 
-new_sentence = reverse_each_word(test_sentance)
+new_sentence = reverse_each(test_sentance)
 
 puts "\nnew_sentence ="
 puts new_sentence
 
-flip_it_back_sentence = reverse_each_word(new_sentence)
-
-
-puts "\nflip_it_back_sentence ="
-puts flip_it_back_sentence
-
-puts "\nflip_it_back_sentence == test_sentance"
-puts flip_it_back_sentence == test_sentance
