849df0f2056ce9708cfa6074053087497e722b20
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-14 18:41:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,24 +4,7 @@ end
 
 
 def reverse_each_word(sentence)
-  # sentence.split.collect {|word| word.reverse}.join(" ")
-  
-  # puts "sentence before split"
-  # puts sentence
-
-  sentence_array = sentence.split
-
-  # puts "\nsentence after split"
-  # puts sentence_array
-  
-  reversed_word_array = sentence_array.collect { |word| word.reverse }
-  
-  # puts "\nreversed_word_array"
-  # puts reversed_word_array
-  
-  reversed_sentence = reversed_word_array.join(" ")
-  
-  return reversed_sentence
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
 
